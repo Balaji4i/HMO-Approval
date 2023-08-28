1c7b1dfe5d43effaa449e4cd99699cd88bbfa5bd
Balaji4i <143384017+Balaji4i@users.noreply.github.com>
2023-08-28 14:39:25 +0530

    HMO Approval commit

---
+++ HMO Approval/View/src/com/view/beans/SearchHmoBB.java
@@ -0,0 +1,79 @@
+package com.view.beans;
+
+import com.view.uiutils.ADFUtils;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.share.ADFContext;
+
+import oracle.jbo.ViewCriteria;
+import oracle.jbo.ViewCriteriaRow;
+import oracle.jbo.ViewObject;
+
+public class SearchHmoBB {
+    public SearchHmoBB() {
+        super();
+    }
+
+    public void onClickStatusCount(ActionEvent actionEvent) {
+        String statusType = ADFContext.getCurrent()
+                                      .getPageFlowScope()
+                                      .get("statusType") == null ? "ALL" : ADFContext.getCurrent()
+                                                                                     .getPageFlowScope()
+                                                                                     .get("statusType")
+                                                                                     .toString();
+
+        ViewObject dashVo = ADFUtils.findIterator("SearchHMOTrxHdr_ROVOIterator").getViewObject();
+        dashVo.applyViewCriteria(null);
+        dashVo.executeQuery();
+        if (!"ALL".equals(statusType)) {
+            ViewCriteria vc = dashVo.createViewCriteria();
+            ViewCriteriaRow vcRow = vc.createViewCriteriaRow();
+            vcRow.setAttribute("HmoAcceptanceFlag", "Y");
+            vc.addRow(vcRow);
+            vcRow.setAttribute("ApprovalStatus", statusType);
+            vc.addRow(vcRow);
+            dashVo.applyViewCriteria(vc);
+            dashVo.executeQuery();
+        } else {
+            ViewCriteria vc = dashVo.createViewCriteria();
+            ViewCriteriaRow vcRow = vc.createViewCriteriaRow();
+            vcRow.setAttribute("HmoAcceptanceFlag", "Y");
+            vc.addRow(vcRow);
+            dashVo.applyViewCriteria(vc);
+            dashVo.executeQuery();
+        }
+    }
+    
+    
+
+    public void onClickExEmpStatusCount(ActionEvent actionEvent) {
+        String statusType = ADFContext.getCurrent()
+                                      .getPageFlowScope()
+                                      .get("statusType") == null ? "ALL" : ADFContext.getCurrent()
+                                                                                     .getPageFlowScope()
+                                                                                     .get("statusType")
+                                                                                     .toString();
+
+        ViewObject dashVo = ADFUtils.findIterator("XxhrHmoTrxHdr_Ex_Emp_ROVO1Iterator").getViewObject();
+        dashVo.applyViewCriteria(null);
+        dashVo.executeQuery();
+        if (!"ALL".equals(statusType)) {
+            ViewCriteria vc = dashVo.createViewCriteria();
+            ViewCriteriaRow vcRow = vc.createViewCriteriaRow();
+            vcRow.setAttribute("HmoAcceptanceFlag", "Y");
+            vc.addRow(vcRow);
+            vcRow.setAttribute("ApprovalStatus", statusType);
+            vc.addRow(vcRow);
+            dashVo.applyViewCriteria(vc);
+            dashVo.executeQuery();
+        } else {
+            ViewCriteria vc = dashVo.createViewCriteria();
+            ViewCriteriaRow vcRow = vc.createViewCriteriaRow();
+            vcRow.setAttribute("HmoAcceptanceFlag", "Y");
+            vc.addRow(vcRow);
+            dashVo.applyViewCriteria(vc);
+            dashVo.executeQuery();
+        }
+    }
+}
